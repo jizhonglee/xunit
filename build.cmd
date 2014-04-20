bd7f93bc3a219e0115ec9aba1abb2824b50596fd
Pranav K <prkrishn@hotmail.com>
2014-04-19 19:22:46 -0700

    Fixing build message

--- build.cmd
+++ build.cmd
@@ -5,7 +5,7 @@ SETLOCAL
 SET CACHED_NUGET=%LocalAppData%\NuGet\NuGet.exe
 
 IF EXIST %CACHED_NUGET% goto copynuget
-echo Downloading latest version of NuGet.exe...
+echo Downloading latest version of NuGet.exe..
 IF NOT EXIST %LocalAppData%\NuGet md %LocalAppData%\NuGet
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest 'https://www.nuget.org/nuget.exe' -OutFile '%CACHED_NUGET%'"
 
